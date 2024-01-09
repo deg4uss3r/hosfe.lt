d70e10a00f498305afc83d5208b885d0647702e9
Ricky Hosfelt <ricky_github@hosfe.lt>
2024-01-08 20:51:12 -0500

    Add the css to request response

--- src/main.rs
+++ src/main.rs
@@ -63,6 +63,12 @@ fn main(req: Request) -> Result<Response, Error> {
                 .with_content_type(mime::TEXT_CSS_UTF_8)
                 .with_body(include_str!("static_site/hosfe.lt/public/css/style.min.dbbe08cb3b07bbce02de1a13a57d4221bb75487e75b0d1a5196a5353f7135921.css")))
         }
+        "/css/style.min.2faed4cf7533c5236bf011e885da2e0c2670dea54d80f6b5ff1f370613f0983a.css" => {
+            Ok(Response::from_status(StatusCode::OK)
+                .with_header(header::ACCESS_CONTROL_ALLOW_ORIGIN, "https://www.hosfe.lt")
+                .with_content_type(mime::TEXT_CSS_UTF_8)
+                .with_body(include_str!("static_site/hosfe.lt/public/css/style.min.2faed4cf7533c5236bf011e885da2e0c2670dea54d80f6b5ff1f370613f0983a.css")))
+        },
         "/js/bundle.min.038214de9d568246fadcfeb06c69349925de3209f332ec123861b6aa031d63c6.js" => {
             Ok(Response::from_status(StatusCode::OK)
                 .with_header(header::ACCESS_CONTROL_ALLOW_ORIGIN, "https://www.hosfe.lt")
