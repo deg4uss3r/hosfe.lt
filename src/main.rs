8fea04fa5bdeda4884f5211226f8f6451205da01
Ricky Hosfelt <ricky_github@hosfe.lt>
2025-05-09 21:06:06 -0400

    Moved all content to KV store

--- src/main.rs
+++ src/main.rs
@@ -33,37 +33,37 @@ fn main(req: Request) -> Result<Response, Error> {
         "/" => {
             Ok(Response::from_status(StatusCode::OK)
                 .with_content_type(mime::TEXT_HTML_UTF_8)
-                .with_body(include_str!("static_site/hosfe.lt/public/index.html")))
+                .with_body(fetch_resource_from_kv("index.html")))
         }
         "/index.xml" => {
             Ok(Response::from_status(StatusCode::OK)
                 .with_content_type(mime::TEXT_XML)
-                .with_body(include_str!("static_site/hosfe.lt/public/index.xml"))) 
+                .with_body(fetch_resource_from_kv("index.xml"))) 
         },
         "/sitemap.xml" => {
             Ok(Response::from_status(StatusCode::OK)
                 .with_content_type(mime::TEXT_XML)
-                .with_body(include_str!("static_site/hosfe.lt/public/sitemap.xml"))) 
+                .with_body(fetch_resource_from_kv("sitemap.xml"))) 
         }
         "/posts/" => {
             Ok(Response::from_status(StatusCode::OK)
                 .with_content_type(mime::TEXT_HTML_UTF_8)
-                .with_body(include_str!("static_site/hosfe.lt/public/posts/index.html"))) 
+                .with_body(fetch_resource_from_kv("posts/index.html"))) 
         },
         "/posts/index.xml" => {
             Ok(Response::from_status(StatusCode::OK)
                 .with_content_type(mime::TEXT_XML)
-                .with_body(include_str!("static_site/hosfe.lt/public/posts/index.xml"))) 
+                .with_body(fetch_resource_from_kv("posts/index.xml"))) 
         }
         "/about/" => {
             Ok(Response::from_status(StatusCode::OK)
                 .with_content_type(mime::TEXT_HTML_UTF_8)
-                .with_body(include_str!("static_site/hosfe.lt/public/about/index.html"))) 
+                .with_body(fetch_resource_from_kv("about/index.html"))) 
         },
         "/resume/" => {
             Ok(Response::from_status(StatusCode::OK)
                 .with_content_type(mime::TEXT_HTML_UTF_8)
-                .with_body(include_str!("static_site/hosfe.lt/public/resume/index.html"))) 
+                .with_body(fetch_resource_from_kv("resume/index.html"))) 
         },
         "/css/style.min.dbbe08cb3b07bbce02de1a13a57d4221bb75487e75b0d1a5196a5353f7135921.css" => {
             Ok(Response::from_status(StatusCode::OK)
@@ -81,50 +81,50 @@ fn main(req: Request) -> Result<Response, Error> {
             Ok(Response::from_status(StatusCode::OK)
                 .with_header(header::ACCESS_CONTROL_ALLOW_ORIGIN, "https://www.hosfe.lt")
                 .with_content_type(mime::APPLICATION_JAVASCRIPT_UTF_8)
-                .with_body(include_str!("static_site/hosfe.lt/public/js/bundle.min.038214de9d568246fadcfeb06c69349925de3209f332ec123861b6aa031d63c6.js")))
+                .with_body(fetch_resource_from_kv("bundle.min.038214de9d568246fadcfeb06c69349925de3209f332ec123861b6aa031d63c6.js")))
         },
         "/js/link-share.min.24409a4f6e5537d70ffc55ec8f9192208d718678cb8638585342423020b37f39.js" => {
             Ok(Response::from_status(StatusCode::OK)
                 .with_header(header::ACCESS_CONTROL_ALLOW_ORIGIN, "https://www.hosfe.lt")
                 .with_content_type(mime::APPLICATION_JAVASCRIPT_UTF_8)
-                .with_body(include_str!("static_site/hosfe.lt/public/js/link-share.min.24409a4f6e5537d70ffc55ec8f9192208d718678cb8638585342423020b37f39.js")))
+                .with_body(fetch_resource_from_kv("link-share.min.24409a4f6e5537d70ffc55ec8f9192208d718678cb8638585342423020b37f39.js")))
         },
         // blog posts go after here
         "/posts/story/" => {
             Ok(Response::from_status(StatusCode::OK)
                 .with_content_type(mime::TEXT_HTML_UTF_8)
-                .with_body(include_str!("static_site/hosfe.lt/public/posts/story/index.html")))
+                .with_body(fetch_resource_from_kv("posts/story/index.html")))
         },
         "/posts/edge/" => {
             Ok(Response::from_status(StatusCode::OK)
                 .with_content_type(mime::TEXT_HTML_UTF_8)
-                .with_body(include_str!("static_site/hosfe.lt/public/posts/edge/index.html")))
+                .with_body(fetch_resource_from_kv("posts/edge/index.html")))
         },
         "/posts/edge2/" => {
             Ok(Response::from_status(StatusCode::OK)
                 .with_content_type(mime::TEXT_HTML_UTF_8)
-                .with_body(include_str!("static_site/hosfe.lt/public/posts/edge2/index.html")))
+                .with_body(fetch_resource_from_kv("posts/edge2/index.html")))
         },
         "/images/edge-post-2_certificate.png" => {
             Ok(Response::from_status(StatusCode::OK)
                 .with_content_type(mime::IMAGE_PNG)
-                .with_body(include_bytes!("static_site/hosfe.lt/public/images/edge-post-2_certificate.png").as_slice()))
+                .with_body(fetch_resource_from_kv("images/edge-post-2_certificate.png")))
         },
         "/posts/adblocker/" => {
             Ok(Response::from_status(StatusCode::OK)
                 .with_content_type(mime::TEXT_HTML_UTF_8)
-                .with_body(include_str!("static_site/hosfe.lt/public/posts/adblocker/index.html")))
+                .with_body(fetch_resource_from_kv("posts/adblocker/index.html")))
         },
         "/images/honeycomb.png" => {
             Ok(Response::from_status(StatusCode::OK)
                 .with_content_type(mime::IMAGE_PNG)
-                .with_body(include_bytes!("static_site/hosfe.lt/public/images/honeycomb.png").as_slice()))
+                .with_body(fetch_resource_from_kv("public/images/honeycomb.png")))
         },
         // Catch all other requests and return a 404.
         _ =>  {
             Ok(Response::from_status(StatusCode::NOT_FOUND)
                 .with_content_type(mime::TEXT_HTML_UTF_8)
-                .with_body(include_str!("static_site/hosfe.lt/public/404.html")))
+                .with_body(fetch_resource_from_kv("404.html")))
         },
     }
 }
